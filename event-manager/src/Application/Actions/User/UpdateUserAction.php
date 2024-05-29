d4ebf7a2e3acaca460adcae927cd826387faebf0
Juan Felipe Jaramillo <juanfelipejaramillolosada@gmail.com>
2024-05-29 08:58:40 -0500

    Password encryption

--- event-manager/src/Application/Actions/User/UpdateUserAction.php
+++ event-manager/src/Application/Actions/User/UpdateUserAction.php
@@ -17,7 +17,8 @@ class UpdateUserAction extends UserAction
         $userId = (int) $this->resolveArg('id');
         $userData = $this->getFormData();
         $validatedUserData = $this->getValidatedUserData($userData);
-
+        $encryptedPassword = password_hash($validatedUserData['password'], PASSWORD_DEFAULT);
+        $validatedUserData['password'] = $encryptedPassword;
         $user = $this->userRepository->updateUser($userId, $validatedUserData);
 
         $this->logger->info("User updated: " . json_encode($user));
